6fd8bf7a3113c82c88325dd04fe610ba10049855
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2011-11-09 16:54:40 +0100

    Read README.rst for the long description

--- setup.py
+++ setup.py
@@ -2,15 +2,10 @@
 
 from distutils.core import setup
 
-long_description = '''
-This module allows you to perform IP subnet calculations, there is support
-for both IPv4 and IPv6 CIDR notation.
-'''
-
 setup(name='ipcalc',
       version='0.4',
       description='IP subnet calculator',
-      long_description=long_description,
+      long_description=file('README.rst').read(),
       author='Wijnand Modderman',
       author_email='python@tehmaze.com',
       url='http://dev.tehmaze.com/projects/ipcalc',
