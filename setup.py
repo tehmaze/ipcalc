1b7756d83b71364a854331d01f47ba991409e73c
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2011-11-10 13:14:20 +0100

    Update home page URL

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='ipcalc',
       long_description=file('README.rst').read(),
       author='Wijnand Modderman',
       author_email='python@tehmaze.com',
-      url='http://dev.tehmaze.com/projects/ipcalc',
+      url='http://tehmaze.github.com/ipcalc',
       packages = [''],
       package_dir = {'': 'src'},
      )
