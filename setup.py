0587c1d0672ea27a904d5699bd4227bf42b94d0f
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2011-11-23 16:00:34 +0100

    Fixed long description

--- setup.py
+++ setup.py
@@ -5,7 +5,41 @@ from distutils.core import setup
 setup(name='ipcalc',
       version='0.4.1',
       description='IP subnet calculator',
-      long_description=file('README.rst').read(),
+      long_description='''
+About
+=====
+
+This module allows you to perform IP subnet calculations, there is support for
+both IPv4 and IPv6 CIDR notation.
+
+Example Usage
+=============
+
+::
+
+    >>> import ipcalc
+    >>> for x in ipcalc.Network('172.16.42.0/30'):
+    ...     print str(x)
+    ...
+    172.16.42.0
+    172.16.42.1
+    172.16.42.2
+    172.16.42.3
+    >>> subnet = ipcalc.Network('2001:beef:babe::/48')
+    >>> print str(subnet.network())
+    2001:beef:babe:0000:0000:0000:0000:0000
+    >>> print str(subnet.netmask())
+    ffff:ffff:ffff:0000:0000:0000:0000:0000
+    >>> '192.168.42.23' in Network('192.168.42.0/24')
+    True
+    >>> long(IP('fe80::213:ceff:fee8:c937'))
+    338288524927261089654168587652869703991L
+
+Bugs/Features
+=============
+
+You can issue a ticket in GitHub: https://github.com/tehmaze/ipcalc/issues
+''',
       author='Wijnand Modderman',
       author_email='python@tehmaze.com',
       url='http://tehmaze.github.com/ipcalc',
